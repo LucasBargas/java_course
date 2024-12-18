f47e1a3182e2ecd9bb02acd7372ed85a667a6248
Lucas Bargas <lucas.bargasoficial@gmail.com>
2024-12-18 02:16:33 -0300

    Types class - completed

---
+++ curse1_java_poo/aula_2_types/types/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+  public static void main(String[] args) {
+    // Integer type
+    int currentYear = 2024;
+    System.out.println(String.format("Estamos no ano de %s", currentYear));
+
+    // Boolean type
+    boolean isAMovie = true;
+    String message = isAMovie ? "This is a movie" : "This's not a movie";
+    System.out.println(message);
+  }
+}
