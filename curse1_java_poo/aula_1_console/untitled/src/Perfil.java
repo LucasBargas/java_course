c24676061a7a3084c3f5bf96c2e45321147bc8d2
Lucas Bargas <lucas.bargasoficial@gmail.com>
2024-12-18 01:35:17 -0300

    first commit

--- curse1_java_poo/aula_1_console/untitled/src/Perfil.java
+++ curse1_java_poo/aula_1_console/untitled/src/Perfil.java
@@ -4,9 +4,11 @@ public class Perfil {
     String surname = "Bargas";
 
     // Template string similar
-    String fullName = String.format("Olá, %s %s!", name, surname);
+    String fullNameVersionOne = String.format("Olá, %s %s!", name, surname);
+    String fullNameVersionTrue = "Olá " + name + " " + surname + "!";
 
-    System.out.println(fullName);
+    System.out.println(fullNameVersionOne);
+    System.out.println(fullNameVersionTrue);
     System.out.println("Tudo bem?");
   }
 }
