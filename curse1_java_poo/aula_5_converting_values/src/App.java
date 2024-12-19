142e81981f63adf5e77732adb895aa2bb185d7ce
Lucas Bargas <lucas.bargasoficial@gmail.com>
2024-12-18 23:58:12 -0300

    Casting class - completed

--- curse1_java_poo/aula_5_converting_values/src/App.java
+++ curse1_java_poo/aula_5_converting_values/src/App.java
@@ -5,5 +5,9 @@ public class App {
     // Converter um tipo double em int - casting
     int movieClassification = (int) media / 2;
     System.out.println(movieClassification);
+
+    int age = 24;
+    double other = (double) age;
+    System.out.println(other);
   }
 }
