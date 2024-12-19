a000878403618971e01904a5a9a63dcc2c961284
Lucas Bargas <lucas.bargasoficial@gmail.com>
2024-12-18 23:55:57 -0300

    Casting class - completed

---
+++ curse1_java_poo/aula_5_converting_values/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    double media = 8.7777;
+
+    // Converter um tipo double em int - casting
+    int movieClassification = (int) media / 2;
+    System.out.println(movieClassification);
+  }
+}
