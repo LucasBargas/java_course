97d3469f9e434c7685b65713e89a1dc3195c442a
Lucas Bargas <lucas.bargasoficial@gmail.com>
2024-12-19 01:07:26 -0300

    Conditional  classes - completed

---
+++ curse1_java_poo/aula_7_conditionals/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+  public static void main(String[] args) {
+    int yearOfRelease = 2024;
+    boolean planIncluded = true;
+    double filmNote = 8.1;
+    String planType = "plus";
+
+    if (yearOfRelease >= 2024) {
+      System.out.println("Lançamento muito curtido!");
+    } else {
+      System.out.println("Filme antigo que vale a pena");
+    }
+
+    if (planIncluded || planType.equals("plus")) {
+      System.out.println("Filme liberado");
+    } else {
+      System.out.println("Deve pagar a locação!");
+    }
+
+    // Switch
+    switch ((int) filmNote) {
+      case 9:
+        System.out.println("Filme bom");
+        break;
+      case 10:
+        System.out.println("Filme excelente");
+        break;
+      default:
+        System.out.println("Filme médio");
+        break;
+    }
+  }
+}
